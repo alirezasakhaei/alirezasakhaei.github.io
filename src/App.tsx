15aa43e8a37416a1363516b096ef2d433dd07453
Alireza <alirezasakhaeirad@gmail.com>
2025-11-09 16:13:08 +0100

    project is set up

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-red-600">
+        Tailwind CSS v4 is working!
+      </h1>
+    </div>
+  )
+}
+
+export default App
